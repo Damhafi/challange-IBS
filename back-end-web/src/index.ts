3e085ef52eef1148b8ebf0410db3a6b22849c03d
Damhafi <yusufmilla@gmail.com>
2023-03-27 01:50:49 -0300

    Update environment file, add Dockerfile and docker-compose, change package.json and index.ts
    Add Dockerfile for front-end-web,
    remove license and Procfile from back-end-web,
    update angular.json for front-end-web
    
    Change: configs and files for Docker and web apps

--- back-end-web/src/index.ts
+++ back-end-web/src/index.ts
@@ -7,7 +7,7 @@ const swaggerFile = require("./swagger/swagger_output.json");
 const prisma = new PrismaClient();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 app.use(cors());
 app.use(express.json());
